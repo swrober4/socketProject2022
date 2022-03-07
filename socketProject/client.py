7f36c9be3afdd69d6215fd27a60bda1b2e761a63
Srothency <srothency@gmail.com>
2022-03-06 21:58:44 -0700

    Update client.py

--- socketProject/client.py
+++ socketProject/client.py
@@ -5,8 +5,8 @@ from time import sleep
 PORT = 31250
 SERVER = "169.254.120.74" # pi ip
 #SERVER = "169.254.131.107" #windows ip
-HEADER = 64 #tells length of message
 FORMAT = 'utf-8'
+MSG_LENGTH = 2048
 ADDR = (SERVER, PORT)
 #game logic stuff
 
@@ -15,28 +15,21 @@ client.connect(ADDR)
 
 def send(msg):
     message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' *  (HEADER - len(send_length))
-    client.send(send_length) #this is the header that tells how long the next message will be
     client.send(message)
-
 def handle_server(): #This is called in its own thread and waits for server responses, then parses them to a handler
     connected = True
     while connected:
-        msg_length = client.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = client.recv(msg_length).decode(FORMAT)
-            print(f"Server sent message {msg}")
-            if msg == 'disconnect':
-                connected = False
+        msg = client.recv(MSG_LENGTH).decode(FORMAT)
+        if msg == 'disconnect':
+            connected = False
+        else:
+            print(msg)
 
 def start():
     thread = threading.Thread(target=handle_server) #client has 2 threads, one for sending messages to server and one for handling responses
     thread.start()
     while(True):
-        message_to_server = input("input (for help type 'help'): ")
+        message_to_server = input()
         send(message_to_server)
 
 start()
